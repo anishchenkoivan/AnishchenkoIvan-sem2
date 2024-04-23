ce564ab8efbf7f4d2806b293a0086c1cdf680629
Ivan Anishchenko <ivananisenko08@gmail.com>
2024-04-23 13:36:22 +0300

    Book purchase service tests

--- book-purchase-service/src/main/java/com/example/bookpurchaseservice/service/BookPurchaseConsumer.java
+++ book-purchase-service/src/main/java/com/example/bookpurchaseservice/service/BookPurchaseConsumer.java
@@ -15,8 +15,14 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 @Service
 public class BookPurchaseConsumer {
@@ -35,6 +41,7 @@ public class BookPurchaseConsumer {
     }
 
     @KafkaListener(topics = {"${payment-request-topic}"})
+    @Transactional(propagation = Propagation.REQUIRED)
     public void makePurchase(String message, Acknowledgment acknowledgment) {
         try {
             BookPaymentRequest parsedMessage = objectMapper.readValue(message, BookPaymentRequest.class);
@@ -45,22 +52,28 @@ public class BookPurchaseConsumer {
                 sendResponse(parsedMessage.bookId(), true);
             } else {
                 sendResponse(parsedMessage.bookId(), false);
-                throw new PurchaseException("Now enough money to purchase book");
             }
-
-            acknowledgment.acknowledge();
         } catch (JsonProcessingException e) {
             LOGGER.error("Failed to parse message {}", e.getMessage());
+        } finally {
+            acknowledgment.acknowledge();
         }
     }
 
     public void sendResponse(Long bookId, boolean success) {
-        String message = null;
         try {
-            message = objectMapper.writeValueAsString(new BookPaymentResponse(bookId, success));
-            CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(responseTopic, message);
+            String message = objectMapper.writeValueAsString(new BookPaymentResponse(bookId, success));
+            CompletableFuture<SendResult<String, String>> sendResult = kafkaTemplate.send(responseTopic, message);
+            sendResult.get(2, TimeUnit.SECONDS);
         } catch (JsonProcessingException e) {
             LOGGER.error("Failed to send rating for book with id = {}", bookId);
+        } catch (ExecutionException e) {
+            LOGGER.error("Couldn't send message to kafka", e);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            LOGGER.error("Unexpected thread interruption", e);
+        } catch (TimeoutException e) {
+            LOGGER.error("Couldn't send message to kafka due to timeout", e);
         }
     }
 }
--- book-purchase-service/src/test/java/com/example/bookpurchaseservice/service/BookPurchaseConsumerTest.java
+++ book-purchase-service/src/test/java/com/example/bookpurchaseservice/service/BookPurchaseConsumerTest.java
@@ -3,38 +3,41 @@ package com.example.bookpurchaseservice.service;
 import com.example.bookpurchaseservice.repository.MoneyRepository;
 import com.example.bookpurchaseservice.service.entity.Money;
 import com.example.bookpurchaseservice.service.request.BookPaymentRequest;
+import com.example.bookpurchaseservice.service.response.BookPaymentResponse;
 import com.example.bookpurchaseservice.service.stabs.KafkaTestConsumer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Testcontainers
 public class BookPurchaseConsumerTest {
     @Container
     @ServiceConnection
-    public static final KafkaContainer KAFKA = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.0"));
+    public static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:13");
 
-    @MockBean
-    private MoneyRepository moneyRepository;
+    @Container
+    @ServiceConnection
+    public static final KafkaContainer KAFKA = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.0"));
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -42,16 +45,79 @@ public class BookPurchaseConsumerTest {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
+    @Autowired
+    private BookPurchaseConsumer bookPurchaseConsumer;
+
+    @Autowired
+    private MoneyRepository moneyRepository;
+
     @Test
     void shouldSuccessfullyMakePurchase() throws JsonProcessingException, InterruptedException {
-        when(moneyRepository.getForUpdate()).thenReturn(Optional.of(new Money(500)));
         String requestData = objectMapper.writeValueAsString(new BookPaymentRequest(1L));
-        KafkaTestConsumer consumer = new KafkaTestConsumer(KAFKA.getBootstrapServers(), "test-book-purchase-group");
-        consumer.subscribe(List.of("test-payment-response"));
+        KafkaTestConsumer consumer = new KafkaTestConsumer(KAFKA.getBootstrapServers(), "test-book-service-group");
 
+        consumer.subscribe(List.of("test-payment-response"));
         kafkaTemplate.send("test-payment-request", requestData);
 
         ConsumerRecords<String, String> records = consumer.poll();
+        assertEquals(0, records.count());
+    }
+
+    @Test
+    @Transactional
+    void shouldMakePurchaseAndWithdrawMoney() throws JsonProcessingException, InterruptedException {
+        String requestData = objectMapper.writeValueAsString(new BookPaymentRequest(1L));
+        KafkaTestConsumer consumer = new KafkaTestConsumer(KAFKA.getBootstrapServers(), "test-book-service-group");
+        consumer.subscribe(List.of("test-payment-response"));
+        assertDoesNotThrow(() -> bookPurchaseConsumer.makePurchase(requestData, new Acknowledgment() {
+            @Override
+            public void acknowledge() {
+            }
+        }));
+
+        Thread.sleep(1000);
+        ConsumerRecords<String, String> records = consumer.poll();
+
         assertEquals(1, records.count());
+        Money money = moneyRepository.getForUpdate().orElseThrow();
+        assertEquals(400, money.getAmount());
+    }
+
+    @Test
+    @Transactional
+    void shouldCancelPurchaseDueToLowBalance() throws JsonProcessingException {
+        KafkaTestConsumer consumer = new KafkaTestConsumer(KAFKA.getBootstrapServers(), "test-book-service-group");
+        consumer.subscribe(List.of("test-payment-response"));
+        for (int i = 0; i < 5; i++) {
+            String requestData = objectMapper.writeValueAsString(new BookPaymentRequest(1L));
+            bookPurchaseConsumer.makePurchase(requestData, new Acknowledgment() {
+                @Override
+                public void acknowledge() {
+                }
+            });
+        }
+        ConsumerRecords<String, String> records = consumer.poll();
+
+        Money money = moneyRepository.getForUpdate().orElseThrow();
+        assertEquals(0, money.getAmount());
+        assertEquals(5, records.count());
+
+        String requestData = objectMapper.writeValueAsString(new BookPaymentRequest(1L));
+        bookPurchaseConsumer.makePurchase(requestData, new Acknowledgment() {
+            @Override
+            public void acknowledge() {
+            }
+        });
+
+        records = consumer.poll();
+        assertEquals(1, records.count());
+        String recordsMessage = "";
+
+        for (ConsumerRecord<String, String> record : records) {
+            recordsMessage = record.value();
+        }
+
+        BookPaymentResponse response = objectMapper.readValue(recordsMessage, BookPaymentResponse.class);
+        assertFalse(response.success());
     }
 }
